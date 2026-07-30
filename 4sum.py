1670ddab8183ccad81b4905828ac0aa73ef153d0
developer-in-world <m.gnanendraworkmail@gmail.com>
2026-07-30 15:23:12 +0530

    4Sum solved with all types of possible solutions

--- 4sum.py
+++ 4sum.py
@@ -5,6 +5,8 @@ class Solution:
         result = set()
         n = len(nums)
 
+        if n < 4: return []
+
         for i in range(n):
             for j in range(i+1, n):
                 for k in range(j+1, n):
@@ -16,9 +18,69 @@ class Solution:
 
         return [list(i) for i in result]
 
+    def betterSolution(self, nums: List[int], target) -> List[List[int]]:
+        myset = set()
+        n = len(nums)
+
+        if n < 4: return []
+
+        for i in range(n):
+            for j in range(i+1, n):
+                hash_set = set()
+                for k in range(j+1, n):
+                    fourth = target - (nums[i] + nums[j] + nums[k])
+                    if fourth in hash_set:
+                        temp = [nums[i], nums[j], nums[k], fourth]
+                        temp.sort()
+                        myset.add(tuple(temp))
+                    hash_set.add(nums[k])
+
+        return [ list(i) for i in myset]
+
+    def optimalSolution(self, nums: List[int], target) -> List[List[int]]:
+        n = len(nums)
+        if n < 4: return []
+        nums.sort()
+
+        result = []
+
+        for i in range(n):
+            if i > 0 and nums[i] == nums[i-1]:
+                continue
+            for j in range(i+1, n):
+                if j > i+1 and nums[j] == nums[j-1]:
+                    continue
+                k = j+1
+                l = n - 1
+
+                while k < l:
+                    total = nums[i] + nums[j] + nums[k] + nums[l]
+
+                    if total == target:
+                        result.append([nums[i], nums[j], nums[k], nums[l]])
+                        k += 1
+                        l -= 1
+                        while k < l and nums[k] == nums[k-1]:
+                            k += 1
+                        while l > k and nums[l] == nums[l+1]:
+                            l -= 1
+                    elif total < target:
+                        k += 1
+                    else:
+                        l -= 1
+
+        return result
+
+
+
+
+
+
+
+
 
 solve = Solution()
 nums = list(map(int, input().split()))
 target = int(input())
-ans = solve.brute_force(nums, target)
+ans = solve.optimalSolution(nums, target)
 print(ans)
