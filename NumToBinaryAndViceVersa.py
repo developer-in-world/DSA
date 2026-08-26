42662e760a182311618672c3a33856cf1ddfee6c
developer-in-world <m.gnanendraworkmail@gmail.com>
2026-08-26 20:37:26 +0530

    Vice cersa

--- NumToBinaryAndViceVersa.py
+++ NumToBinaryAndViceVersa.py
@@ -15,3 +15,20 @@ def convert2Binary(num: int) -> str:
 print(convert2Binary(9))
 print(convert2Binary(13))
 print(convert2Binary(10))
+
+
+def convert2Decimal(x: str) -> int:
+    decimal_number = 0
+    power = 0
+    index = len(x) - 1
+    
+    while index >= 0:
+        num = int(x[index]) * (2**power)
+        decimal_number += num
+        
+        index -= 1
+        power += 1
+    
+    return decimal_number
+
+print(convert2Decimal("1101"))
