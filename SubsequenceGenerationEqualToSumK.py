1ac3d753466c6f6e0a7ccc072b3d41a04530bf49
developer-in-world <m.gnanendraworkmail@gmail.com>
2026-09-05 11:49:57 +0530

    Optimal approach, instead of generating all the combinations and checking the sum at last increases the computation so we try to save some by taking the variable named total to keep track of the sum and goo through the tree if required only else leave it.

--- SubsequenceGenerationEqualToSumK.py
+++ SubsequenceGenerationEqualToSumK.py
@@ -2,7 +2,7 @@ class Solution:
     def generateSubsequenceEqualToK(self, nums, target):
         result = []
         subset = []
-        self.__solve(nums, target, 0, subset, result)
+        self.__solveOptimal(0, 0, subset, nums, target, result)
         return result
     
     def __solve(self, nums, target, index, subset, result):
@@ -14,6 +14,22 @@ class Solution:
         self.__solve(nums, target, index+1, subset, result)
         subset.pop()
         self.__solve(nums, target, index+1, subset, result)
+    
+    def __solveOptimal(self, index, total, subset, nums, target, result):
+        if total == target:
+            result.append(subset.copy())
+            return
+        elif total > target:
+            return
+        elif index >= len(nums):
+            return
+        subset.append(nums[index])
+        sum = total + nums[index]
+        self.__solveOptimal(index+1, sum, subset, nums, target, result)
+        e = subset.pop()
+        sum = sum - e # this is the optimal solution, where we dont generate all the possible ones and save some computation
+        self.__solveOptimal(index+1, sum, subset, nums, target, result)
+        
         
 solver = Solution()
 arr = list(map(int, input().split()))
